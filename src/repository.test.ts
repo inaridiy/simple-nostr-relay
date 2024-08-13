c3d1f9d74c95e94a8eb7f3b953b2f40a720c7788
inaridiy <inainainariinainari@gmail.com>
2024-08-13 19:41:02 +0900

    test: nip26 writing test

--- src/repository.test.ts
+++ src/repository.test.ts
@@ -1,18 +1,24 @@
 import * as schema from "@/database";
+import { schnorr } from "@noble/curves/secp256k1";
+import { sha256 } from "@noble/hashes/sha256";
+import { bytesToHex } from "@noble/hashes/utils";
 import Database from "better-sqlite3";
+import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import { finalizeEvent, generateSecretKey } from "nostr-tools/pure";
+import { finalizeEvent, generateSecretKey, getPublicKey } from "nostr-tools/pure";
 import { beforeAll, describe, expect, it } from "vitest";
+import { verifyDelegation } from "./nostr/nips26";
 import { createRepository } from "./repository";
 import type { Event } from "./types/core";
 
 describe("Event Repository", () => {
+  let db: ReturnType<typeof drizzle<typeof schema>>;
   let repository: ReturnType<typeof createRepository>;
 
   beforeAll(async () => {
     const sqlite = new Database(":memory:");
-    const db = drizzle(sqlite, { schema });
+    db = drizzle(sqlite, { schema });
     await migrate(db, { migrationsFolder: "./drizzle" });
     repository = createRepository(db, { enableNIP26: true });
   });
@@ -72,5 +78,31 @@ describe("Event Repository", () => {
       const savedEvent = await repository.queryEventById(event.id);
       expect(savedEvent).toMatchObject(event);
     });
+  });
+  it("should create an event with delegation", async () => {
+    const delegator = generateSecretKey();
+    const delegatee = generateSecretKey();
+
+    const delegationTokenMessage = `nostr:delegation:${getPublicKey(delegatee)}:kind=1`;
+    const token = schnorr.sign(sha256(delegationTokenMessage), delegator);
+
+    const event = finalizeEvent(
+      {
+        kind: 1,
+        created_at: Math.floor(Date.now() / 1000),
+        tags: [["delegation", getPublicKey(delegator), "kind=1", bytesToHex(token)]],
+        content: "hello",
+      },
+      delegatee,
+    );
+
+    expect(verifyDelegation(event as unknown as Event)).toBe(true);
+
+    await repository.saveEvent(event as unknown as Event);
+    const savedEvent = await repository.queryEventById(event.id);
+    expect(savedEvent).toMatchObject(event);
+
+    const rawSaved = await db.query.events.findFirst({ where: eq(schema.events.id, event.id) });
+    expect(rawSaved?.detegator).toBe(getPublicKey(delegator));
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   resolve: {
     alias: {
+      "@/validators": "/src/generated/typia",
       "@": "/src",
+    },
+  },
+  test: {
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
     },
   },
 });
