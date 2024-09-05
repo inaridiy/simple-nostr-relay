658b5c3e823b34f63ea4ad4136323d1750dbc73a
inaridiy <inainainariinainari@gmail.com>
2024-09-05 17:00:40 +0900

    feat:

--- otel/baselime.ts
+++ otel/baselime.ts
@@ -2,6 +2,7 @@ import { BaselimeSDK } from "@baselime/node-opentelemetry";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 
 const sdk = new BaselimeSDK({
+  service: process.env.SERVICE_NAME,
   instrumentations: [getNodeAutoInstrumentations({ "@opentelemetry/instrumentation-fs": { enabled: false } })],
 });
 
